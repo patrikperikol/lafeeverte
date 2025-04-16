0555596a50a08a59068300b6855d748af2651489
patrik <38130262+patrikperikol@users.noreply.github.com>
2025-04-16 18:25:16 +0300

    create initial db pool

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import router from "./routes/router.js";
+import { pool } from "./persistence/db.js";
 
 const app = express();
 const port = 3000;
 
 app.use(router);
+
+pool.query('SELECT NOW()')
+    .then(res => {
+        console.log('Database connected:', res.rows[0]);
+    })
+    .catch(err => {
+        console.error('Database connection error:', err)
+    });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
