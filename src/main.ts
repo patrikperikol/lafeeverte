346e9ba4482e9dd8226a83887f73fd11a40a873c
patrik <38130262+patrikperikol@users.noreply.github.com>
2025-04-17 14:44:03 +0300

    add db init to main file, add graceful shutdown

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
 import express from "express";
 import router from "./routes/router.js";
-import { pool } from "./persistence/db.js";
+import { db } from "./persistence/db.js";
 
 const app = express();
 const port = 3000;
 
 app.use(router);
 
-pool.query('SELECT NOW()')
-    .then(res => {
-        console.log('Database connected:', res.rows[0]);
-    })
-    .catch(err => {
-        console.error('Database connection error:', err)
-    });
+db.init();
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+process.on("SIGINT", async () => {
+    console.log("Server shutting down...");
+    await db.shutdown();
+    server.close(() => process.exit(0));
+});
+
