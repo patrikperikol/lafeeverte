f46ef1bf96009f0aef7a1ec3f36f602f071be978
patrik <38130262+patrikperikol@users.noreply.github.com>
2025-04-17 14:43:39 +0300

    clean up database module

--- src/persistence/db.ts
+++ src/persistence/db.ts
@@ -1,9 +1,39 @@
-import pg from "pg";
+import pg, { Pool, PoolClient } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL
-});
+let pool: Pool | undefined;
+
+function init() {
+    if (!pool) {
+        if (!process.env.DATABASE_URL) {
+            throw new Error("DATABASE_URL is not defined");
+        }
+
+        pool = new pg.Pool({
+            connectionString: process.env.DATABASE_URL
+        });
+    }
+}
+
+async function shutdown() {
+    if (pool) {
+        await pool.end();
+        pool = undefined;
+    }
+}
+
+async function connect(): Promise<{ client: PoolClient }> {
+    if (!pool) {
+        throw new Error("Db not connected");
+    }
+    return { client: await pool.connect() };
+}
+
+export const db = {
+    init,
+    shutdown,
+    connect
+}
 
